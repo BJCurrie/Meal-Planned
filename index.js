c6e3e4e792d465e60ba422a0b891903045176157
Ben Currie <69808500+BJCurrie@users.noreply.github.com>
2022-10-10 15:27:18 +0100

    added recipe1 / basic variables

---
+++ index.js
@@ -0,0 +1,27 @@
+recipe1 = {
+    recipe: 'Pesto Pasta',
+    ingredients: [
+        {
+        ingredient: 'Pesto',
+        quantity: 50,
+        measurement: 'g',
+        },
+        {
+        ingredient: 'Pasta',
+        quantity: 200,
+        measurement: 'g',
+        },
+        {
+        ingredient: 'Cream',
+        quantity: 30,
+        measurement: 'g',
+        }
+    ]
+}
+
+const greeting = 'Hello fellow time efficient GitHub home chef.'
+const open = 'Your weekly meal plan has been randomised for you as follows:'
+const pantry = 'Please ensure you have the following ingredients:'
+
+
+
