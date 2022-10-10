0be213a951ea5668ed9176382380784e74ad8353
Ben Currie <69808500+BJCurrie@users.noreply.github.com>
2022-10-10 15:50:34 +0100

    made change to greeting

--- index.js
+++ index.js
@@ -19,7 +19,34 @@ recipe1 = {
     ]
 }
 
-const greeting = 'Hello fellow time efficient GitHub home chef.'
+recipe2 = {
+    recipe: 'Garlic Tofu and Broccoli',
+    ingredients: [
+        {
+        ingredient: 'Tofu',
+        quantity: 300,
+        measurement: 'g',
+        },
+        {
+        ingredient: 'Broccoli',
+        quantity: 1,
+        measurement: 'head',
+        },
+        {
+        ingredient: 'Garlic',
+        quantity: 4,
+        measurement: 'cloves',
+        },
+        {
+        ingredient: 'Rice',
+        quantity: 1,
+        measurement: 'cup',
+        }
+    ]
+}
+
+
+const greeting = 'Hello fellow time efficient GitHub chef,'
 const open = 'Your weekly meal plan has been randomised for you as follows:'
 const pantry = 'Please ensure you have the following ingredients:'
 
