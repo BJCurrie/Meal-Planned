c5a6003b40387b90cfe4865cd01f85d6558d85e7
Ben Currie <69808500+BJCurrie@users.noreply.github.com>
2022-10-10 16:25:47 +0100

    added new recipes and comments

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
+// Stored recipes
 recipe1 = {
     recipe: 'Pesto Pasta',
     ingredients: [
         {
         ingredient: 'Pesto',
         quantity: 50,
-        measurement: 'g',
+        measurement: 'g'
         },
         {
         ingredient: 'Pasta',
         quantity: 200,
-        measurement: 'g',
+        measurement: 'g'
         },
         {
         ingredient: 'Cream',
         quantity: 30,
-        measurement: 'g',
+        measurement: 'g'
         }
     ]
 }
@@ -25,30 +26,228 @@ recipe2 = {
         {
         ingredient: 'Tofu',
         quantity: 300,
-        measurement: 'g',
+        measurement: 'g'
         },
         {
         ingredient: 'Broccoli',
         quantity: 1,
-        measurement: 'head',
+        measurement: 'head'
         },
         {
         ingredient: 'Garlic',
         quantity: 4,
-        measurement: 'cloves',
+        measurement: 'cloves'
         },
         {
         ingredient: 'Rice',
         quantity: 1,
-        measurement: 'cup',
+        measurement: 'cups'
         }
     ]
 }
 
+recipe3 = {
+    recipe: 'Vegetable Topped Potato',
+    ingredients: [
+        {
+        ingredient: 'Chickpeas',
+        quantity: 200,
+        measurement: 'g'
+        },
+        {
+        ingredient: 'Baking Potatoes',
+        quantity: 2,
+        measurement: ''
+        },
+        {
+        ingredient: 'Hummus',
+        quantity: 100,
+        measurement: 'g'
+        },
+        {
+        ingredient: 'Garlic',
+        quantity: 3,
+        measurement: 'cloves'
+        },
+        {
+        ingredient: 'Spinach',
+        quantity: 75,
+        measurement: 'g'
+        },
+        {
+        ingredient: 'Yellow Onions',
+        quantity: 1,
+        measurement: ''
+        }
+    ]
+}
 
+recipe4 = {
+    recipe: 'Mushroom Ramen',
+    ingredients: [
+        {
+        ingredient: 'Mushrooms',
+        quantity: 150,
+        measurement: 'g'
+        },
+        {
+        ingredient: 'Stock Cubes',
+        quantity: 4,
+        measurement: ''
+        },
+        {
+        ingredient: 'Spring Onions',
+        quantity: 4,
+        measurement: ''
+        },
+        {
+        ingredient: 'Garlic',
+        quantity: 2,
+        measurement: 'cloves'
+        },
+        {
+        ingredient: 'Noodles',
+        quantity: 200,
+        measurement: 'g'
+        }
+    ]
+}
+
+recipe5 = {
+    recipe: 'Classic Burgers',
+    ingredients: [
+        {
+        ingredient: 'Burger Patties',
+        quantity: 2,
+        measurement: ''
+        },
+        {
+        ingredient: 'Burger Buns',
+        quantity: 2,
+        measurement: ''
+        },
+        {
+        ingredient: 'Mixed Salad',
+        quantity: 40,
+        measurement: 'g'
+        },
+        {
+        ingredient: 'Gherkins',
+        quantity: 2,
+        measurement: ''
+        }
+    ]
+}
+
+recipe6 = {
+    recipe: 'Chickpea Curry',
+    ingredients: [
+        {
+        ingredient: 'Chickpeas',
+        quantity: 200,
+        measurement: 'g'
+        },
+        {
+        ingredient: 'Tomatoes',
+        quantity: 100,
+        measurement: 'g'
+        },
+        {
+        ingredient: 'Yellow Onions',
+        quantity: 2,
+        measurement: ''
+        },
+        {
+        ingredient: 'Garlic',
+        quantity: 4,
+        measurement: 'cloves'
+        },
+        {
+        ingredient: 'Lemons',
+        quantity: 1,
+        measurement: ''
+        },
+        {
+        ingredient: 'Rice',
+        quantity: 1,
+        measurement: 'cups'
+        }
+    ]
+}
+
+recipe7 = {
+    recipe: 'Cauliflower Curry',
+    ingredients: [
+        {
+        ingredient: 'Cauliflower',
+        quantity: 1,
+        measurement: 'head'
+        },
+        {
+        ingredient: 'Coconut Milk',
+        quantity: 400,
+        measurement: 'g'
+        },
+        {
+        ingredient: 'Yellow Onions',
+        quantity: 1,
+        measurement: ''
+        },
+        {
+        ingredient: 'Garlic',
+        quantity: 3,
+        measurement: 'cloves'
+        },
+        {
+        ingredient: 'Rice',
+        quantity: 1,
+        measurement: 'cups'
+        }
+    ]
+}
+
+recipe8 = {
+    recipe: 'Tofu Fried Rice',
+    ingredients: [
+        {
+        ingredient: 'Tofu',
+        quantity: 300,
+        measurement: 'g'
+        },
+        {
+        ingredient: 'Sweetcorn',
+        quantity: 100,
+        measurement: 'g'
+        },
+        {
+        ingredient: 'Yellow Onions',
+        quantity: 1,
+        measurement: ''
+        },
+        {
+        ingredient: 'Spring Onions',
+        quantity: 4,
+        measurement: ''
+        },
+        {
+        ingredient: 'Garlic',
+        quantity: 4,
+        measurement: 'cloves'
+        },
+        {
+        ingredient: 'Rice',
+        quantity: 1,
+        measurement: 'cups'
+        }
+    ]
+}
+
+// Stored strings to be printed out to the user
 const greeting = 'Hello fellow time efficient GitHub chef,'
 const open = 'Your weekly meal plan has been randomised for you as follows:'
 const pantry = 'Please ensure you have the following ingredients:'
+
+
 
 
 
